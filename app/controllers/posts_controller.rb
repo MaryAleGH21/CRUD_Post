6d947317100945ffc85ae083093b09c041d35722
Alejandra <marialesr.17@gmail.com>
2021-04-15 16:35:48 -0600

    view show ready

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,5 +6,12 @@ class PostsController < ApplicationController
       format.json {render json: @posts.to_json}
     end
   end
+
+  def show
+    @post = Post.find(params[:id])
+    respond_to do |format|
+      format.json {render json: @post.to_json}
+    end
+  end
 end
 
